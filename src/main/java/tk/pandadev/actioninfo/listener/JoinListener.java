0b2ae15d20fc45c742a696735e75522bcebfa0d9
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-05-10 13:12:28 +0200

    stop runnable when no players are online

--- src/main/java/tk/pandadev/actioninfo/listener/JoinListener.java
+++ src/main/java/tk/pandadev/actioninfo/listener/JoinListener.java
@@ -1,26 +1,50 @@
 package tk.pandadev.actioninfo.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import tk.pandadev.actioninfo.Main;
+import tk.pandadev.actioninfo.utils.ActionBar;
 
 public class JoinListener implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event){
+    public void onJoin(PlayerJoinEvent event) {
         FileConfiguration config = Main.getInstance().getConfig();
         Player player = event.getPlayer();
-        if (config.get(player.getUniqueId().toString() + ".active") == null){
-            config.set(player.getUniqueId().toString() + ".active", false);
-            config.set(player.getUniqueId().toString() + ".tps", false);
-            config.set(player.getUniqueId().toString() + ".mspt", false);
-            config.set(player.getUniqueId().toString() + ".cpu", false);
-            config.set(player.getUniqueId().toString() + ".ram", false);
+        if (config.get(player.getUniqueId() + ".active") == null) {
+            config.set(player.getUniqueId() + ".active", false);
         }
-
+        if (config.get(player.getUniqueId() + ".tps") == null) {
+            config.set(player.getUniqueId() + ".tps", false);
+        }
+        if (config.get(player.getUniqueId() + ".mspt") == null) {
+            config.set(player.getUniqueId() + ".mspt", false);
+        }
+        if (config.get(player.getUniqueId() + ".cpu") == null) {
+            config.set(player.getUniqueId() + ".cpu", false);
+        }
+        if (config.get(player.getUniqueId() + ".ram") == null) {
+            config.set(player.getUniqueId() + ".ram", false);
+        }
+        if (config.get(player.getUniqueId() + ".ping") == null) {
+            config.set(player.getUniqueId() + ".ping", false);
+        }
         Main.getInstance().saveConfig();
+        if (Bukkit.getOnlinePlayers().size() == 1) {
+            ActionBar.startActionBar();
+        }
     }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        if (Bukkit.getOnlinePlayers().size() == 0) {
+            ActionBar.stopActionBar();
+        }
+    }
+
 }
